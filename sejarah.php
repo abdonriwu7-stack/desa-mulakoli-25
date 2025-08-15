803aaaba81c456283dbb9cc184e161c22cb68e4e
abdonriwu7-stack <abdonriwu7@gmail.com>
2025-08-14 22:13:41 -0700

    Add files via upload

---
+++ sejarah.php
@@ -0,0 +1,24 @@
+<?php
+$title = "Sejarah Desa";
+include("template/user/header.php");
+include("template/user/navbar.php"); ?>
+
+<div class="container">
+
+    <h4 class="mb-3">Sejarah Desa Mulakoli</h4>
+    <img src="assets/img/KANTOR.jpg" class="w-100 mb-3" alt="">
+    <h6 class="text-uppercase">Sejarah Singkat Desa Mulakoli </h6>
+    <p>
+Seiring waktu, Dusun I (Kopi) menjadi pusat pertumbuhan penduduk Desa Mulakoli. Dari sinilah mulai berkembang berbagai tradisi adat yang menjadi ciri khas masyarakat setempat, seperti Ritual Ka Buku (syukur panen) dan Melo Sa’o (masuk rumah baru), yang terus dijaga dan dilestarikan hingga saat ini. Nama Mulakoli pun melekat dan diwariskan dari generasi ke generasi, bukan hanya sebagai identitas wilayah, tetapi juga sebagai simbol awal mula kehidupan masyarakatnya.
+<br><br>
+Secara Etimologi, kata Mulakoli berasal dari gabungan dua kata dalam bahasa daerah Nagekeo, yaitu "Mula" yang berarti awal atau sumber, dan "Koli" yang berarti tanah lapang atau permukiman. Makna ini menggambarkan Mulakoli sebagai "tanah awal permukiman", mengacu pada wilayah pertama yang dihuni oleh leluhur masyarakat setempat di kaki Gunung Ebulobo.
+<br><br>
+Menurut cerita rakyat yang dituturkan secara turun-temurun, pemukiman awal di Dusun I berada di area lapang dan subur yang menjadi pusat berkumpulnya masyarakat adat. Tempat ini bukan hanya digunakan untuk bercocok tanam, tetapi juga menjadi pusat pelaksanaan upacara adat dan musyawarah. Dari sinilah nama Mulakoli melekat dan diwariskan dari generasi ke generasi.
+<br><br>
+Pada dasarnya, wilayah Desa Mulakoli dahulu mencakup area yang cukup luas, meliputi beberapa dusun yang kini telah mengalami pemekaran. Dalam perkembangan administrasi, terutama pada era reformasi, sebagian wilayah Desa Mulakoli dimekarkan menjadi desa-desa baru. Namun, nama Mulakoli tetap digunakan secara resmi dalam konteks pemerintahan dan dikenal luas sebagai pusat kebudayaan serta pintu gerbang pendakian Gunung Ebulobo.
+</p>
+</div>
+
+
+
+<?php include("template/user/footer.php") ?>
